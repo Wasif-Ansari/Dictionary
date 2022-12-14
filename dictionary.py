0a819d205f2d87035bde946a3e18223985bd9911
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-14 19:06:19 +0530

    creating a button and means label

--- dictionary.py
+++ dictionary.py
@@ -10,4 +10,11 @@ entry = Entry(root,font=("Times New Roman", 15, "bold"))
 entry.grid(row=1,column=2)
 entry.place(x=10,y=10,width=600)
 
+buttons = Button(root , text="FIND MEANING")
+buttons.grid(row=4 , column=2)
+buttons.place(x=225,y=45,width=150)
+
+means = Label(root,text="None",font=("Times New Roman",10,"bold" ),bg="white",fg="blue",wraplength=300, justify="center")
+means.place(x=10,y=100,height=200,width=600)
+
 root.mainloop()
